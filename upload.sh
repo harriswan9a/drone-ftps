2b9a4fb677516c7a6a8af1c620707ba9577b8b38
Harris <harriswan9a@gmail.com>
2018-01-17 00:50:25 +0800

    test drone

--- upload.sh
+++ upload.sh
@@ -22,6 +22,10 @@ if [ -z "$PLUGIN_SRC_DIR" ]; then
     PLUGIN_SRC_DIR="/"
 fi
 
+if [ -z "$PLUGIN_SSL_ALLOW" ]; then
+    PLUGIN_SSL_ALLOW="true"
+fi
+
 PLUGIN_EXCLUDE_STR=""
 PLUGIN_INCLUDE_STR=""
 
@@ -34,4 +38,4 @@ for i in "${in_arr[@]}"; do
     PLUGIN_INCLUDE_STR="$PLUGIN_INCLUDE_STR -x $i"
 done
 
-lftp -c "open -u $PLUGIN_USERNAME,$FTP_PASSWORD $PLUGIN_HOSTNAME; set ftp:ssl-force $PLUGIN_SECURE; set ftp:ssl-protect-data $PLUGIN_SECURE; mirror -R $PLUGIN_INCLUDE_STR $PLUGIN_EXCLUDE_STR $(pwd)$PLUGIN_SRC_DIR $PLUGIN_DEST_DIR"
+lftp -c "open -u $PLUGIN_USERNAME,$FTP_PASSWORD $PLUGIN_HOSTNAME; set ftp:ssl-allow $PLUGIN_SSL_ALLOW; set ftp:ssl-force $PLUGIN_SECURE; set ftp:ssl-protect-data $PLUGIN_SECURE; mirror -R $PLUGIN_INCLUDE_STR $PLUGIN_EXCLUDE_STR $(pwd)$PLUGIN_SRC_DIR $PLUGIN_DEST_DIR"
